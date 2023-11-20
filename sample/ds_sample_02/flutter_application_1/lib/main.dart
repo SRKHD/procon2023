f75acb94841082e217a44af4d1aa3911679c93af
SRKHD <srkhd.2023@gmail.com>
2023-11-20 21:49:24 +0900

    Flutter＋Firebaseサンプルアプリ(DS) Firebase コンソールでの準備 cloud_firestore プラグインのインストールとインポート

--- sample/ds_sample_02/flutter_application_1/lib/main.dart
+++ sample/ds_sample_02/flutter_application_1/lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
 import 'firebase_options.dart';
 
 void main() async {
@@ -17,9 +19,74 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  final TextEditingController _controller = TextEditingController();
+
+  final _list = List.generate(10, (index) => 'test $index');
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final reverseList = _list.reversed.toList();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Container(
+                height: double.infinity,
+                alignment: Alignment.topCenter,
+                child: ListView.builder(
+                  itemCount: reverseList.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Center(
+                      child: Text(
+                        reverseList[index],
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    autofocus: true,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _list.add(_controller.text);
+                      _controller.clear();
+                    });
+                  },
+                  child: const Text('送信'),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
