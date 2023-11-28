380dab5b0cee4022f092ea8e9dc2a76b7dc69afc
da-sasaki <da-sasaki@kentem.co.jp>
2023-11-28 12:43:26 +0900

    UI修正(D.S)

--- project/bodquest_2023/lib/presentation/page/weight_page.dart
+++ project/bodquest_2023/lib/presentation/page/weight_page.dart
@@ -2,15 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class WeightPage extends StatelessWidget {
+class WeightPage extends StatefulWidget {
   const WeightPage({Key? key}) : super(key: key);
 
   @override
+  State<WeightPage> createState() => _WeightPageState();
+}
+
+class _WeightPageState extends State<WeightPage> {
+  final TextEditingController _controller = TextEditingController();
+
+  final _list = List.generate(10, (index) => 'test $index');
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final reverseList = _list.reversed.toList();
     return Center(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
+          Expanded(
+            child: Container(
+              height: double.infinity,
+              alignment: Alignment.topCenter,
+              child: ListView.builder(
+                itemCount: reverseList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Center(
+                    child: Text(
+                      reverseList[index],
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
           TextField(
             textAlign: TextAlign.right,
             decoration: const InputDecoration(hintText: '体重を入力'),
