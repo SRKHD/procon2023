6ed51584f73fe7336369cd2d603b15a7b3d893ae
SRKHD <srkhd.2023@gmail.com>
2023-12-02 09:43:21 +0900

    domain/factory : エンティティを生成するためのインターフェースを定義。UserFactoryを追加。RugUserクラスは型だけ定義。

---
+++ project/bodquest_2023/lib/domain/factory/user_factory.dart
@@ -0,0 +1,15 @@
+import '../../infrastructure/model/rug_user.dart';
+import '../entity/user.dart';
+
+abstract interface class IUserFactory {
+  /// [User]を生成する
+  User create({
+    required String name,
+    required String gender,
+    required String thumbnail,
+    required DateTime birthday,
+  });
+
+  /// [RugUser]から[User]を生成する
+  User createFromModel(RugUser user);
+}
