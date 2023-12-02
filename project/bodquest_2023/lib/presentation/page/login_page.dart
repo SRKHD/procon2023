aa85cb6133e225b3446b0f104cbd32c69bbf6313
fu-suzuki <srkhd.2023@gmail.com>
2023-12-02 16:21:08 +0900

    【アカウント作成】起動後にログインページに飛ぶように実装＋ログインページにアカウント作成ボタンを作成

--- project/bodquest_2023/lib/presentation/page/login_page.dart
+++ project/bodquest_2023/lib/presentation/page/login_page.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
-class LogInPage extends StatelessWidget {
-  const LogInPage({Key? key}) : super(key: key);
-
+class LogInPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return SizedBox();
-  }
+  State<LogInPage> createState() => _LogInPage();
 }
+
+class _LogInPage extends State<LogInPage> {
+
+ @override
+ Widget build(BuildContext context) {
+
+    return Scaffold(
+        body: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                // todo：ログイン画面
+                Padding(
+                  padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
+                  // アカウント作成ボタン
+                  child:ElevatedButton(
+                  child: const Text('アカウントを作成する'),
+                  onPressed: () {
+                    // todo
+                  }),
+                  )
+              ],
+            ),
+        ),
+    );
+ }
+} 
