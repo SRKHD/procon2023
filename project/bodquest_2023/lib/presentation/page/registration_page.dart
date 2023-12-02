8d1f1b732bb3397f46724fba4f281ef3491b8838
fu-suzuki <srkhd.2023@gmail.com>
2023-12-02 16:37:33 +0900

    【アカウント作成】アカウント作成ページのUI作成。

--- project/bodquest_2023/lib/presentation/page/registration_page.dart
+++ project/bodquest_2023/lib/presentation/page/registration_page.dart
@@ -15,10 +15,48 @@ class _RegistrationPageState extends State<RegistrationPage> {
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                  Padding(
+                Padding(
                   padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 30.0),
                   child:Text('新規アカウントの作成',
-                    style: TextStyle(fontSize: 20))
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
+                ),
+
+                // メールアドレスの入力フォーム
+                Padding(
+                  padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
+                  child:TextFormField(
+                    decoration: InputDecoration(labelText: "メールアドレス"),
+                    onChanged: (String value) {
+                      // todo
+                    },
+                  )
+                ),
+
+                // パスワードの入力フォーム
+                Padding(
+                  padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 10.0),
+                  child:TextFormField(
+                    decoration: InputDecoration(
+                      labelText: "パスワード（8～20文字）"
+                    ),
+                    obscureText: true,  // パスワードが見えないようにする
+                    maxLength: 20,  // 入力可能な文字数
+                  ),
+                ),
+
+                // 登録失敗時のエラーメッセージ
+                 // todo
+                   
+                ButtonTheme(
+                  minWidth: 350.0,
+                  // height: 100.0,
+                  child:ElevatedButton(
+                    child: Text('登録',
+                      style: TextStyle(fontWeight: FontWeight.bold),),
+                    onPressed: (){
+                      // todo
+                    }
+                  ),
                 ),
               ],
             ),
