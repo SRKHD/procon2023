08a887a644bbf5117cb5817a4751da063aec260b
SRKHD <srkhd.2023@gmail.com>
2023-12-04 21:48:15 +0900

    コメント削除

--- project/bodquest_2023/lib/infrastructure/datasource/firestore_weights_datasource_impl.dart
+++ project/bodquest_2023/lib/infrastructure/datasource/firestore_weights_datasource_impl.dart
@@ -9,49 +9,14 @@ class FirestoreWeightsDataSourceImpl implements IFirestoreWeightsDataSource {
   Future<RugGetWeightsResponse> getWeights({int results = 10}) async {
     RugGetWeightsResponse result = RugGetWeightsResponse(results: []);
     const userId = 'srkhd.2023@gmail.com';
-    /*
-    final weightsRef = FirebaseFirestore.instance
-        .collection('weights') // コレクションID
-        .where('userId', isEqualTo: userId);
 
-    weightsRef.get().then((snapshot) => {
-          snapshot.docs.map((e) => {
-                print(e.data()),
-                result.results.add(RugWeight.fromJson(e.data()))
-              })
-          //   if (snapshot.docs.length > 0)
-          //     {result = RugGetWeightsResponse.fromJson(docSnapshot.data())}
-          //   else
-          //     {
-          //       throw NetworkException(
-          //           'FirestoreWeightsDataSourceImpl getWeights() "/"')
-          //     }
-          // });
-          // return result;
-          // });
-        });
-        */
-    print('aaa ddddd');
     await FirebaseFirestore.instance
         .collection('weights')
         .where('userId', isEqualTo: userId)
         .get()
         .then((value) => {
-              /*
-              print(value.size),
-              print(value.docs.length),
-              value.docs.forEach((e) {
-                // print(e.exists),
-                // print(e.data().toString()),
-                // e.data(),
-                RugGetWeightsResponse.fromJson(e.data());
-              })
-              */
               for (var doc in value.docs)
-                {
-                  // RugGetWeightsResponse.fromJson(doc.data()),
-                  result.results.add(RugWeight.fromJson(doc.data()))
-                }
+                {result.results.add(RugWeight.fromJson(doc.data()))}
             });
 
     return result;
