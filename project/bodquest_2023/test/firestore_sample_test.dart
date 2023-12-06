d10eb15e5a77e09e822a7a5b3b7aab0fcb8f59ff
da-sasaki <da-sasaki@kentem.co.jp>
2023-12-06 17:30:31 +0900

    firestoreのテスト更新

--- project/bodquest_2023/test/firestore_sample_test.dart
+++ project/bodquest_2023/test/firestore_sample_test.dart
@@ -1,4 +1,5 @@
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
 // import 'package:flutter_test/flutter_test.dart';
 
 // 参考
@@ -10,8 +11,41 @@ void main() async {
   await instance.collection('users').add({
     'username': 'Bob',
   });
-  final snapshot = await instance.collection('users').get();
+  await instance.collection('users').add({
+    'username': 'Tom',
+  });
+  final snapshot = await instance
+      .collection('users')
+      .where('username', isEqualTo: 'Bob')
+      .get();
   print(snapshot.docs.length); // 1
   print(snapshot.docs.first.get('username')); // 'Bob'
   print(instance.dump());
+
+  final snapshot2 = instance
+      .collection('users')
+      .where('username', isEqualTo: 'Bob')
+      .snapshots();
+
+  snapshot2.listen((qrySnapshot) {
+    final List<String> names = qrySnapshot.docs.map((document) {
+      Map<String, dynamic> data = document.data();
+      String s = data['username'];
+      return s;
+    }).toList();
+    print('before...');
+    try {
+      for (var name in names) {
+        print('aaa:$name');
+      }
+      print('after...');
+    } catch (e) {
+      print(e);
+    }
+  });
+  // print(await snapshot2.length); // 1
+  // snapshot2.forEach((element) {
+  //   print(element.toString());
+  // }};
+  print('end...');
 }
