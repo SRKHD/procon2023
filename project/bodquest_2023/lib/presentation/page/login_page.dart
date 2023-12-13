a82f4f9d6461533b8e8ba17084a6e4b972f568ea
fu-suzuki <srkhd.2023@gmail.com>
2023-12-13 23:13:22 +0900

    【ログイン機能】コード整形

--- project/bodquest_2023/lib/presentation/page/login_page.dart
+++ project/bodquest_2023/lib/presentation/page/login_page.dart
@@ -15,122 +15,117 @@ class LogInPage extends StatefulWidget {
 class _LogInPage extends State<LogInPage> {
   String _email = "";
   String _password = "";
-  String _infoText = ""; 
+  String _infoText = "";
 
   late UserCredential _result;
   late User _user;
-  
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-        body: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            // メールアドレスの入力フォーム
+            Padding(
+                padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
+                child: TextFormField(
+                  decoration: InputDecoration(labelText: "メールアドレス"),
+                  onChanged: (String value) {
+                    _email = value;
+                  },
+                )),
 
-                // メールアドレスの入力フォーム
-                Padding(
-                  padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
-                  child:TextFormField(
-                    decoration: InputDecoration(
-                      labelText: "メールアドレス"
-                    ),
-                    onChanged: (String value) {
-                      _email = value;
-                    },
-                  )
-                ),
+            // パスワードの入力フォーム
+            Padding(
+              padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 10.0),
+              child: TextFormField(
+                decoration: InputDecoration(labelText: "パスワード（8～20文字）"),
+                obscureText: true, // パスワードが見えないようにする
+                maxLength: 20, // 入力可能な文字数
+                maxLengthEnforcement:
+                    MaxLengthEnforcement.enforced, // 入力可能な文字数の制限を超える場合の挙動の制御
+                onChanged: (String value) {
+                  _password = value;
+                },
+              ),
+            ),
 
-                // パスワードの入力フォーム
-                Padding(
-                  padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 10.0),
-                  child:TextFormField(
-                    decoration: InputDecoration(
-                      labelText: "パスワード（8～20文字）"
-                    ),
-                    obscureText: true,  // パスワードが見えないようにする
-                    maxLength: 20,  // 入力可能な文字数
-                    maxLengthEnforcement: MaxLengthEnforcement.enforced,// 入力可能な文字数の制限を超える場合の挙動の制御
-                    onChanged: (String value) {
-                      _password = value;
-                    },
-                  ),
-                ),
-                
-                // ログイン失敗時のエラーメッセージ
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20.0, 0, 20.0, 0.0),
-                  child:Text(_infoText,
-                    style: TextStyle(color: Colors.red),),
-                ),
+            // ログイン失敗時のエラーメッセージ
+            Padding(
+              padding: EdgeInsets.fromLTRB(20.0, 0, 20.0, 0.0),
+              child: Text(
+                _infoText,
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
 
-                // ログインボタン
-                Padding(
-                  padding: EdgeInsets.fromLTRB(25.0, 5.0, 25.0, 50),
-                  child: ElevatedButton(
-                    child: const Text('ログイン'),
-                  onPressed: () async{
-                    try{
+            // ログインボタン
+            Padding(
+              padding: EdgeInsets.fromLTRB(25.0, 5.0, 25.0, 50),
+              child: ElevatedButton(
+                  child: const Text('ログイン'),
+                  onPressed: () async {
+                    try {
                       // ログイン
-                      _result = await FirebaseAuth.instance.signInWithEmailAndPassword(
-                        email: _email,
-                        password: _password);
-                        
+                      _result = await FirebaseAuth.instance
+                          .signInWithEmailAndPassword(
+                              email: _email, password: _password);
+
                       // ログイン成功
                       // ホーム画面に遷移
                       _user = _result.user!;
-                      if (context.mounted) {Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
-                        )
-                      );
+                      if (context.mounted) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const MyHomePage(
+                                  title: 'Flutter Demo Home Page'),
+                            ));
                       }
-                    }
-                    on FirebaseAuthException catch (e)
-                    {
+                    } on FirebaseAuthException catch (e) {
                       // ログインに失敗した場合
                       setState(() {
                         _infoText = AuthException(e.code).toString();
                       });
                     }
                   }),
-                ),
-                                      
-                Padding(
-                  padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
-                  // アカウント作成ボタン
-                  child:ElevatedButton(
+            ),
+
+            Padding(
+              padding: EdgeInsets.fromLTRB(25.0, 0, 25.0, 0),
+              // アカウント作成ボタン
+              child: ElevatedButton(
                   child: const Text('アカウントを作成する'),
-                  onPressed: (){
+                  onPressed: () {
                     Navigator.of(context).push(
                       MaterialPageRoute(
                         fullscreenDialog: true,
                         builder: (BuildContext context) => RegistrationPage(),
                       ),
                     );
-                    }),
-                  ),
+                  }),
+            ),
 
-                  // 開発用
-                  Padding(
-                  padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 0),
-                  child:ElevatedButton(
+            // 開発用
+            Padding(
+              padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 0),
+              child: ElevatedButton(
                   child: const Text('開発用：ログインスキップ'),
-                  onPressed: (){
+                  onPressed: () {
                     Navigator.of(context).push(
                       MaterialPageRoute(
                         fullscreenDialog: true,
-                        builder: (BuildContext context) => const MyHomePage(title: 'Flutter Demo Home Page'),
+                        builder: (BuildContext context) =>
+                            const MyHomePage(title: 'Flutter Demo Home Page'),
                       ),
                     );
                   }),
-                  )
-                  
-              ],
-            ),
+            )
+          ],
         ),
+      ),
     );
- }
-} 
+  }
+}
