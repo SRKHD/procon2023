239889e01df3a90b5212d33d2543d33b77102659
SRKHD <srkhd.2023@gmail.com>
2023-12-23 16:13:56 +0900

    [feat] HomeのViewを作成

---
+++ project/bodquest_2023/lib/presentation/component/home/pace_text.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../../theme/sizes.dart';
+
+class PaceText extends StatelessWidget {
+  const PaceText({super.key, required this.expectedWeight});
+  final double expectedWeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('30日後の予想体重'),
+        Text(
+          '$expectedWeight kg',
+          style: TextStyle(fontSize: RawSize.p60),
+        ),
+      ],
+    );
+  }
+}
--- project/bodquest_2023/lib/presentation/page/home_page.dart
+++ project/bodquest_2023/lib/presentation/page/home_page.dart
@@ -1,19 +1,42 @@
-import 'package:bodquest_2023/domain/value/evaluation_rank.dart';
-import 'package:bodquest_2023/presentation/component/chara_image.dart';
 import 'package:flutter/material.dart';
+
+import '../../domain/value/evaluation_rank.dart';
+import '../component/control/item_evaluation_panel.dart';
+import '../component/home/chara_image.dart';
+import '../component/home/pace_text.dart';
+import '../component/home/rank_text.dart';
+import '../theme/sizes.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final image = Center(
-      child: CharaImage(rank: EvaluationRank.g),
-    );
-
+    const tempRank = EvaluationRank.g;
+    const tempWeight = 67.5;
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [image],
+      children: [
+        RankText(
+          rank: tempRank,
+        ),
+        PaceText(
+          expectedWeight: tempWeight,
+        ),
+        ItemEvaluationPanel(
+          title: '体重',
+          perceentBarGraphWidth: RawSize.p200,
+          perceentBarGraphHeight: RawSize.p32,
+          perceentBarGraphValue: 30,
+        ),
+        ItemEvaluationPanel(
+          title: '食事',
+          perceentBarGraphWidth: RawSize.p200,
+          perceentBarGraphHeight: RawSize.p32,
+          perceentBarGraphValue: 90,
+        ),
+        CharaImage(rank: tempRank)
+      ],
     );
   }
 }
