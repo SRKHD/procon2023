48820d73294230d970c1014a562c5d9e238abd58
SRKHD <srkhd.2023@gmail.com>
2023-12-23 18:12:48 +0900

    [refactor] インターフェースにプロバイダー定義が残っていたので修正

--- project/bodquest_2023/lib/infrastructure/datasource/firestore/users_datasource.dart
+++ project/bodquest_2023/lib/infrastructure/datasource/firestore/users_datasource.dart
@@ -1,11 +1,4 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import '../../model/firestore/user/rug_get_users_response.dart';
-import 'users_datasource_impl.dart';
-
-final fireStoreUsersDataSourceProvider = Provider<IFirestoreUsersDataSource>(
-  (ref) => FirestoreUsersDataSourceImpl(),
-);
 
 abstract interface class IFirestoreUsersDataSource {
   Future<RugGetUsersResponse> getUsers({int results});
--- project/bodquest_2023/lib/infrastructure/datasource/firestore/weights_datasource.dart
+++ project/bodquest_2023/lib/infrastructure/datasource/firestore/weights_datasource.dart
@@ -1,12 +1,4 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import '../../model/firestore/weight/rug_get_weights_response.dart';
-import 'weights_datasource_impl.dart';
-
-final fireStoreWeightsDataSourceProvider =
-    Provider<IFirestoreWeightsDataSource>(
-  (ref) => FirestoreWeightsDataSourceImpl(),
-);
 
 abstract interface class IFirestoreWeightsDataSource {
   Stream<RugGetWeightsResponse> getWeights(String userId);
