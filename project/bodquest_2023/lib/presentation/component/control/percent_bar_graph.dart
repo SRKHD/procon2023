ea9a0ee3d614fbb805e128046cc4de23c77847a7
SRKHD <srkhd.2023@gmail.com>
2024-01-01 16:18:27 +0900

    [feat] メインに運動パラメータ追加。メインから一覧表示できるように対応

--- project/bodquest_2023/lib/presentation/component/control/percent_bar_graph.dart
+++ project/bodquest_2023/lib/presentation/component/control/percent_bar_graph.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 
 class PerceentBarGraph extends StatelessWidget {
-  const PerceentBarGraph(
-      {super.key,
-      required this.width,
-      required this.height,
-      required this.percentValue});
+  const PerceentBarGraph({
+    super.key,
+    required this.width,
+    required this.height,
+    required this.percentValue,
+    required this.onPressed,
+  });
 
   final double width;
   final double height;
   final int percentValue;
 
+  /// コールバック
+  final VoidCallback onPressed;
+
   @override
   Widget build(BuildContext context) {
     final buttonwidth = width * (percentValue / 100);
     final valueButton = ElevatedButton(
-      onPressed: () {},
+      onPressed: onPressed,
       style: ElevatedButton.styleFrom(
           backgroundColor: Colors.green,
           minimumSize: Size(buttonwidth, height)),
