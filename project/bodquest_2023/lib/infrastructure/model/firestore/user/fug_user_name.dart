64f59822342d90c124831183c7a4fdb807d43d5e
fu-suzuki <srkhd.2023@gmail.com>
2024-01-07 14:49:11 +0900

    ユーザー名：今回はfirst/lastに分けずfullNameで対応

--- project/bodquest_2023/lib/infrastructure/model/firestore/user/fug_user_name.dart
+++ project/bodquest_2023/lib/infrastructure/model/firestore/user/fug_user_name.dart
@@ -1,19 +1,22 @@
 class FugUserName {
-  final String title;
-  final String first;
-  final String last;
+  //final String title;
+  //final String first;
+  //final String last;
+  final String userName;
 
   FugUserName({
-    required this.first,
-    required this.last,
-    required this.title,
+    //required this.first,
+    //required this.last,
+    //required this.title,
+    required this.userName,
   });
 
-  factory FugUserName.fromJson(Map<String, dynamic> json) {
+  factory FugUserName.fromJson(String json) {
     return FugUserName(
-      first: json['first'],
-      last: json['last'],
-      title: json['title'],
+      //first: json['first'],
+      //last: json['last'],
+      //title: json['title'],
+      userName: json,
     );
   }
 }
