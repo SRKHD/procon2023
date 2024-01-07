846c090d8d7fd99d6aacbcf19a2f47ea093312ba
SRKHD <srkhd.2023@gmail.com>
2024-01-07 09:27:56 +0900

    [feat] コンパイルできるようになったので一旦コミット

---
+++ project/bodquest_2023/lib/presentation/page/sample/yolov5/yolov5_sample_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//import 'home_screen.dart';
+
+import 'RunModelByCameraDemo.dart';
+
+//import 'RunModelByImageDemo.dart';
+
+class YOLOv5Sample extends ConsumerWidget {
+  YOLOv5Sample({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    //return HomeScreen();
+    return RunModelByCameraDemo();
+  }
+}
