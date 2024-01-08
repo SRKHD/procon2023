a44f5f27adb0caf524b3847c62a7520d1983e657
ka-ozawa <ka-ozawa@kentem.co.jp>
2024-01-08 21:15:08 +0900

    MealRegisterKindを作成

--- project/bodquest_2023/lib/presentation/component/component_types.dart
+++ project/bodquest_2023/lib/presentation/component/component_types.dart
@@ -44,3 +44,22 @@ enum MealKind {
     }
   }
 }
+
+enum MealRegisterKind {
+  meal('meal'), // 食事登録
+  recipe('recipe'); // レシピ登録
+
+  final String value;
+  const MealRegisterKind(this.value);
+
+  factory MealRegisterKind.from(String value) {
+    switch (value) {
+      case 'meal':
+        return MealRegisterKind.meal;
+      case 'recipe':
+        return MealRegisterKind.recipe;
+      default:
+        throw UnimplementedError();
+    }
+  }
+}
