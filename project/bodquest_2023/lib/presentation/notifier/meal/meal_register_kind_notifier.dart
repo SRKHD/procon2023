c1424a60728701314e3dda9259cf52ad43d9dce3
sh-sano <srkhd.2023@gmail.com>
2024-01-09 21:57:16 +0900

    ファイル名を変更。

--- project/bodquest_2023/lib/presentation/notifier/meal/meal_register_kind_notifier.dart
+++ project/bodquest_2023/lib/presentation/notifier/meal/meal_register_kind_notifier.dart
@@ -2,7 +2,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../component/component_types.dart';
 
-part 'mealregister_kind_notifier.g.dart';
+part 'meal_register_kind_notifier.g.dart';
 
 /// 食事登録の種類を状態管理
 @riverpod
